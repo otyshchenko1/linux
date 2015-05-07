fa860e2a8c46adccba48c683ae5ed6504b5d5386
Hans Verkuil <hverkuil@xs4all.nl>
2015-05-07 03:26:21 -0300

    rcar-vin: use monotonic timestamps
    Even though the rcar-vin driver tells userspace that it will give a monotonic
    timestamp, it is actually using gettimeofday. Replace this with a proper
    monotonic timestamp.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Acked-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/platform/soc_camera/rcar_vin.c
+++ drivers/media/platform/soc_camera/rcar_vin.c
@@ -916,7 +916,7 @@ static irqreturn_t rcar_vin_irq(int irq, void *data)
 						 priv->field;
 			priv->queue_buf[slot]->v4l2_buf.sequence =
 						 priv->sequence++;
-			do_gettimeofday(&priv->queue_buf[slot]->
+			v4l2_get_timestamp(&priv->queue_buf[slot]->
 						v4l2_buf.timestamp);
 			vb2_buffer_done(priv->queue_buf[slot],
 						 VB2_BUF_STATE_DONE);
