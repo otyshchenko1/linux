ec4cbbebb953340d5d4310bcb53b7af31b5760be
Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
2022-03-21 16:09:13 +0200

    workaround patch provided by Juergen (V3)
    To be squashed with:
    xen/grants: support allocating consecutive grants
    
    Signed-off-by: Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>

--- drivers/xen/grant-table.c
+++ drivers/xen/grant-table.c
@@ -257,13 +257,14 @@ static int get_free_seq(unsigned int count)
 				continue;
 		}
 
-		*gnttab_free_tail_ptr = from;
 		while (from < to) {
 			*last = from;
 			last = __gnttab_entry(from);
 			gnttab_last_free = from;
 			from++;
 		}
+		if (to < gnttab_size)
+			gnttab_free_tail_ptr = __gnttab_entry(to - 1);
 	}
 
 	*last = GNTTAB_LIST_END;
@@ -296,7 +297,7 @@ static int get_free_entries_seq(unsigned int count)
 	}
 
 	ret = *gnttab_free_tail_ptr;
-	*gnttab_free_tail_ptr = gnttab_entry(ret + count - 1);
+	*gnttab_free_tail_ptr = gnttab_entry(ret + count);
 	gnttab_free_count -= count;
 	if (!gnttab_free_count)
 		gnttab_free_tail_ptr = NULL;
