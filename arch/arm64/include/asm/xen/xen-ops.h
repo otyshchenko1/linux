483feda2793042aecfc7d1d1a0c3955555436e70
Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
2022-04-04 12:41:46 +0300

    [WIP] Avoid modifications to the virtio code V1
    TODO: The virtio-pci part is missing.
    
    Signed-off-by: Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>

---
+++ arch/arm64/include/asm/xen/xen-ops.h
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_ARM_XEN_OPS_H
+#define _ASM_ARM_XEN_OPS_H
+
+#include <linux/virtio_config.h>
+#include <xen/swiotlb-xen.h>
+#include <xen/xen-ops.h>
+
+static inline void xen_setup_dma_ops(struct device *dev)
+{
+	if (xen_swiotlb_detect())
+		dev->dma_ops = &xen_swiotlb_dma_ops;
+
+#ifdef CONFIG_XEN_VIRTIO
+	if (arch_has_restricted_virtio_memory_access() && xen_is_virtio_device(dev))
+		xen_virtio_setup_dma_ops(dev);
+#endif
+}
+
+#endif /* _ASM_ARM_XEN_OPS_H */
